8eccd47cea7df032df93358264f64ace1eba63f9
Eon Viljoen <Eon.Viljoen@dariel.co.za>
2025-04-19 20:15:33 +0200

    corrected names for document storing

--- WordCollectionApi/Models/Word.cs
+++ WordCollectionApi/Models/Word.cs
@@ -7,13 +7,13 @@ namespace WordCollectionApi.Models
     public class Word
     {
         [BsonId]
-        [BsonElement("WordId"), BsonRepresentation(MongoDB.Bson.BsonType.Int64)]
+        [BsonElement("id"), BsonRepresentation(MongoDB.Bson.BsonType.Int64)]
         [JsonPropertyName("id")]
         public int WordId { get; set; }
-        [BsonElement("WordValue"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        [BsonElement("word"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
         [JsonPropertyName("word")]
         public required string WordValue { get; set; }
-        [BsonElement("WordType"), BsonRepresentation(MongoDB.Bson.BsonType.Int64)]
+        [BsonElement("type"), BsonRepresentation(MongoDB.Bson.BsonType.Int64)]
         [JsonPropertyName("type")]
         public required WordTypeEnum WordType { get; set; }
     }
