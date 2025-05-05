510d0ff53afd6212fb5ea00362e3ea0ba64ebbf3
Eon Viljoen <Eon.Viljoen@dariel.co.za>
2025-05-05 12:24:34 +0200

    forgot to save changes to environment file

--- WordCollectionWebApp/src/environments/environment.development.ts
+++ WordCollectionWebApp/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     apiBaseUrl: 'https://wordcollectionapi.onrender.com',
     wordEnpoint: '/WordCollectionApi/Word',
     clientId: 'Ov23liMkbTCok4VA0VTA',
-    redirectUri: 'http://localhost:4200/WordCollection/oauth/github'
+    frontEndLoginUri: 'https://eonviljoen.github.io/WordCollection/auth/github/login'
 };
